642a2f07a94eedab77912da7cd0081b2b7341045
viktor <viktorhristovski629@gmail.com>
2025-01-31 14:14:29 +0100

    feat: TodoBot Client

--- src/__main__.py
+++ src/__main__.py
@@ -1,7 +1,16 @@
-import discord
+from discord import Intents
+from dotenv import load_dotenv
+from .TodoBotClient import TodoBotClient
+import os
 
-intents=discord.Intents(
-    message_content=True,
-)
-client=discord.Client(intents=intents)
-# client.connect(,reconnect=True)
+load_dotenv()
+bot_token: str | None = os.getenv("BOT_TOKEN")
+
+if bot_token is None:
+    print("Bot token identification not provided")
+    exit(1)
+
+
+intent=Intents(message_content=True)
+client=TodoBotClient(intents=intent)
+client.run(bot_token)
